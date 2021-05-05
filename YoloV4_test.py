f219bd81b7363a2b236952ed9612453920085ebf
salehahmad <sos2sua@gmail.com>
2021-05-05 16:12:24 +0100

    Added functionality to save the detected objects as image files.

--- YoloV4_test.py
+++ YoloV4_test.py
@@ -10,6 +10,8 @@ SKIP_FRAME_COUNT = 6
 DETECT_ALL_CLASSES = False
 desiredClasses = ["person", "car", "motorbike", "bus", "truck", "sheep"]
 
+SAVE_DETECTED_OBJECT_IMAGES=True
+
 LOOKING_FOR = []
 classNames = []
 with open("model/classList.txt", "r") as f:
@@ -31,6 +33,8 @@ model.setInputParams(size=(416, 416), scale=1 / 255, swapRB=True)
 skip = 0
 
 writer = 0
+
+objectCount = 0
 while cv2.waitKey(1) < 1:
     (grabbed, frame) = vc.read()
     if not grabbed:
@@ -56,6 +60,13 @@ while cv2.waitKey(1) < 1:
         if not DETECT_ALL_CLASSES:
             if id[0] not in LOOKING_FOR:
                 continue
+
+        if SAVE_DETECTED_OBJECT_IMAGES:
+            objectCount += 1
+            x, y, w, h = box
+            objectROI = frame[y:y+h, x:x+w]
+            cv2.imwrite("objects/"+classNames[id[0]]+str(objectCount)+".jpg", objectROI)
+
         cv2.rectangle(frame, box, (255,0,0), 2)
         cv2.putText(frame, classNames[id[0]], (box[0], box[1] - 5), cv2.FONT_ITALIC, 0.8, (255, 255, 255), 2)
 
